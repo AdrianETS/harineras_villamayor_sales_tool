82688377e361902453102bd12e92778f46ab5fae
Merche <mercedes.rodriguez@movicoders.com>
2020-06-03 10:49:30 +0200

    Feature/18.- Creación pantalla listado productos
    Diseño

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppContext } from './../context/ContextProvider';
 import Navbar from "./Navbar";
 import { Link } from 'react-router-dom';
+import trigo from './../images/trigo.png';
 
 class ProductsList extends React.Component {
 
@@ -10,11 +11,15 @@ class ProductsList extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            productsList: ""
+        }
     }
 
     componentDidMount() {
        this.context.checkToken(this);
-       this.context.getProductsList(this.props.history);
+       this.context.getProductsList(this.props.history)
+       .then(products => this.setState({productsList: products}))
 
     }
 
@@ -23,16 +28,24 @@ class ProductsList extends React.Component {
             return (<div>
                 <Navbar name = {this.context.userName} />
                 <h5 style = {{marginLeft: "100px"}}>List of products:</h5>
-                <br /><div style = {{marginLeft: "100px", fontFamily: "sans-serif"}}>
-                    <ul>
-                        {this.context.productsList && this.context.productsList.map(productos =>
-                            <div>
-                                <li>
-                                    {productos.nombre_comercial} 
-                                </li> 
+                <div style = {{marginLeft: "100px", fontFamily: "sans-serif"}}>
+                 
+                    <div className="container">
+                        <div className="d-flex flex-wrap">
+
+                            {this.state.productsList && this.state.productsList.map(productos =>
+                            <div className="productCard">
+                                <div><img src={trigo}/></div>
+                                <div>{productos.nombre_comercial}</div>
+                                <div><button className="btn-primary" type="button">Add product</button></div> 
                             </div>
-                        )}
-                    </ul>
+                            )}
+  
+                        </div>
+                    </div>
+                                
+                       
+                   
                 </div>
             </div>);
         }
