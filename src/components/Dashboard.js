3e9f6cbbce67251ffde97f82ce4de77aecc4e458
Merche <mercedes.rodriguez@movicoders.com>
2020-06-17 17:47:18 +0200

    Feature/18.- Creación pantalla listado productos
    modificar estilos boton añadir producto y  funcionalidad del boton

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -40,8 +40,8 @@ class Dashboard extends React.Component {
                         alt="First slide"
                         />
                         <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3></h3>
+                        <p></p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
@@ -52,8 +52,8 @@ class Dashboard extends React.Component {
                         />
 
                         <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3></h3>
+                        <p></p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
@@ -64,8 +64,8 @@ class Dashboard extends React.Component {
                         />
 
                         <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3></h3>
+                        <p></p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
