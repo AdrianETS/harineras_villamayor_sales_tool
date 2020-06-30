ec2c583d1a1133124fc44c1a68d35a7f714313cb
Adrian <adrian.belver@etservices.eu>
2020-06-30 17:33:30 +0200

    Minor fix
    Product details muestra ahora el precio con descuento (si procede)

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -11,14 +11,15 @@ class ProductDetail extends React.Component {
         super(props);
         this.state = {
             selectedProduct: {},
-            id: this.props.location?.state?.id
+            //id: this.props.location?.state?.id,
+            producto: this.props.location.state.producto
         }
     }
 
     componentDidMount() {
         this.context.checkToken(this);
-        this.context.getProductInfo(this.props.history, this.state.id)
-            .then(product => this.setState({ selectedProduct: product[0] }))       
+        //this.context.getProductInfo(this.props.history, this.state.id)
+           // .then(product => this.setState({ selectedProduct: product[0] }))       
     }
 
     handleNumberChange(event) {
@@ -29,7 +30,7 @@ class ProductDetail extends React.Component {
         return (
             <div><Navbar name={this.context.userName} />
             <div className="container">
-                <h4 className=""> Product {this.state.selectedProduct.nombre_comercial}</h4>
+                <h4 className=""> Product {this.state.producto && this.state.producto.nombre_comercial}</h4>
                 <h5 className="container_within_navbar"> Product's details</h5>
 
                 <table id="clientDetails" /*className="table table-bordered"*/>
@@ -39,12 +40,12 @@ class ProductDetail extends React.Component {
                         <th colSpan="2">Product Details</th>   
                     </tr>
                     <tr>
-                        <td className="subRow" rowSpan="5"><img className="imgProduct" src={"/images/productXl/" + this.state.selectedProduct.img}/></td>
-                        <th className="subTitle">Product Id</th><td className="subRow">{this.state.selectedProduct.id}</td></tr>
-                        <tr><th className="subTitle">Tradename</th><td className="subRow">{this.state.selectedProduct.nombre_comercial}</td></tr>
-                        <tr><th className="subTitle">Price</th><td className="subRow">{this.state.selectedProduct.precio} Euros</td></tr>
-                        <tr><th className="subTitle">Unit of measurement</th><td className="subRow">{this.state.selectedProduct.unidad_medida}</td></tr>
-                        <tr><th className="subTitle">Other data</th><td className="subRow">{this.state.selectedProduct.otros_datos}</td></tr>
+                        <td className="subRow" rowSpan="5"><img className="imgProduct" src={"/images/productXl/" + this.state.producto.img}/></td>
+                        <th className="subTitle">Product Id</th><td className="subRow">{this.state.producto && this.state.producto.id}</td></tr>
+                        <tr><th className="subTitle">Tradename</th><td className="subRow">{this.state.producto && this.state.producto.nombre_comercial}</td></tr>
+                        <tr><th className="subTitle">Price</th><td className="subRow">{this.state.producto &&  this.state.producto.precio} Euros</td></tr>
+                        <tr><th className="subTitle">Unit of measurement</th><td className="subRow">{this.state.producto &&  this.state.producto.unidad_medida}</td></tr>
+                        <tr><th className="subTitle">Other data</th><td className="subRow">{this.state.producto &&  this.state.producto.otros_datos}</td></tr>
                         
                 </table>
                 <br/>
@@ -54,8 +55,8 @@ class ProductDetail extends React.Component {
                                 onChange={(event)=>this.handleNumberChange(event)} aria-label="Search" />
                     <button type="button" 
                             className="btnSubmitSale" 
-                            onClick= {()=>this.context.addProductToCart({id: this.state.selectedProduct.id, nombre_comercial: this.state.selectedProduct.nombre_comercial, 
-                            precio: this.state.selectedProduct.precio, unidad_medida: this.state.selectedProduct.unidad_medida, cantidad: parseInt(this.state.quantitySelected)})}>
+                            onClick= {()=>this.context.addProductToCart({id: this.state.producto.id, nombre_comercial: this.state.producto.nombre_comercial, 
+                            precio: this.state.producto.precio, unidad_medida: this.state.producto.unidad_medida, cantidad: parseInt(this.state.quantitySelected)})}>
                             <i class="fas fa-plus" style = {{color: "white", fontSize: "16px"}} ></i>
                     </button>
                     </div>
